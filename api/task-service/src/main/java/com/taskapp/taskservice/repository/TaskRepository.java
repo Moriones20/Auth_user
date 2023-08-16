4acf9cc634f33b228915161b641bb3e56458aff1
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-08-15 21:22:53 -0500

    CRUD tasks completed

--- api/task-service/src/main/java/com/taskapp/taskservice/repository/TaskRepository.java
+++ api/task-service/src/main/java/com/taskapp/taskservice/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.taskapp.taskservice.repository;
 
-public interface TaskRepository {
+import java.util.Optional;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.taskapp.taskservice.model.Task;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+    Optional<Task> findByTitle(String title);
 }
